6bcc4739cfbadacd1de8d451fced0dfb10e5eea2
Chenhw2017 <1272617071@qq.com>
2018-02-08 02:54:17 +0800

    spring表单

---
+++ src/pojo/Product.java
@@ -0,0 +1,36 @@
+package pojo;
+
+public class Product {
+
+	private int id;
+	private String name;
+	private float price;
+	private String category;
+	public String getCategory() {
+		return category;	
+	}
+	public void setCategory(String category) {
+		this.category=category;
+		
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	
+	
+}
